3d12245014752c4391481d2e9e69eccfdd873fed
Massoud <massoudemami80@yahoo.fr>
2022-10-02 08:42:49 +0200

    modif2

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'stripe_key' => 'pk_test_DuXAPj9VX2bRENW50llmkpuO006SHcVC1R',
-    'stripe_secret' => 'sk_test_51ExW5vF4YoZUm0etZRNLQ8CQOXVNqDG6arsbcMcV9ghYBFjUAu9A8AH2l6xEXT95uXlcnjyYaQDaLtCqZh2ePzSM00ZpAIptzp'
+    'stripe_key' => '',
+    'stripe_secret' => ''
 ];
